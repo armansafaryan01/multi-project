15bd22ab311037db61fdf53658418f797f818f08
Erik7778 <ergrig81@gmail.com>
2022-08-08 21:59:25 +0400

    add-reailize

--- src/components/Home/component/cards/index.jsx
+++ src/components/Home/component/cards/index.jsx
@@ -7,12 +7,12 @@ const Cards = () => {
 
      return (
           <div className="global-cont-cards">
-               <div className="box-card"></div>
-               <div className="box-card"></div>
-               <div className="box-card"></div>
-               <div className="box-card"></div>
-               <div className="box-card"></div>
-
+               <div className="box-card" style={{ backgroundImage: `url(${require("..//..//..//..//assets/icons/fruity.jpg")})` }}></div>
+               <div className="box-card" style={{ backgroundImage: `url(${require("..//..//..//..//assets/icons/exchange.jpg")})` }}></div>
+               <div className="box-card" style={{ backgroundImage: `url(${require("..//..//..//..//assets/icons/blue.jpg")})` }}></div>
+               <div className="box-card" style={{ backgroundImage: `url(${require("..//..//..//..//assets/icons/national.jpg")})` }}></div>
+               <div className="box-card" style={{ backgroundImage: `url(${require("..//..//..//..//assets/icons/media.jpg")})` }}></div>
+               <div className="box-card" style={{ backgroundImage: `url(${require("..//..//..//..//assets/icons/apply.jpg")})` }}></div>
 
           </div>
 
