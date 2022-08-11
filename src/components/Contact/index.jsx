755afe45386f0caa390c2b21e34ab17b9a792675
Erik7778 <ergrig81@gmail.com>
2022-08-11 07:03:48 +0400

    add-realize

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -3,13 +3,45 @@ import "./style.css"
 
 
 const Contact = () => {
-    
-return (
-      <div className='contact-cont'>
-    
-           
+
+  return (
+    <div className='contact-cont'>
+      <div className="for-sections-link">
+        <h4>Home</h4>
+        <span>/</span>
+        <h4 id="shop-list">Contact</h4>
       </div>
+      <div className='name-cont'>
+        <h1>CONTACT US-------------------------------------------------------------------------------------------------</h1>
+      </div>
+      <div className='cont-for-inputs'>
+        <label>
+          <input name='Name' type="Name" placeholder='Your Name' />
+        </label>
+        <label>
+          <input name='Email' type="Email" placeholder='Your Email' />
+        </label>
+        <label>
+          <input type="text" placeholder='Subject' />
+        </label>
+        <label>
+          <input id='message' type="text" placeholder='Message' />
+        </label>
+        <button>Send Message</button>
+      </div>
+      <div className='cont-for-lokation'>
+        <div className='box-for-iframe'>
+          <iframe className='iframe' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d918.07650149806!2d-43.38914!3d-23.012534000000002!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa62b20f8f46e46bd!2sQuiosque%20Sol%20Da%20Barra!5e0!3m2!1sen!2sth!4v1655650210755!5m2!1sen!2sth" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+        </div>
+        <div className='box-for-lokation'>
+          <p>123 Street, New York, USA</p>
+          <p>info@example.com</p>
+          <p>+012 345 67890</p>
+        </div>
+      </div>
+    </div>
   )
 };
-  
+
 export default Contact;
--- src/components/ShopDetail/index.jsx
+++ src/components/ShopDetail/index.jsx
@@ -15,24 +15,24 @@ import blueshirt from "..//..//assets/icons/blueshirt.jpg"
 
 const products = [
   {
-       image: camera,
-       name: "Product Name Goes Here",
-       price: "$123  $123.00",
+    image: camera,
+    name: "Product Name Goes Here",
+    price: "$123  $123.00",
   },
   {
-       image: blueshirt,
-       name: "Product Name Goes Here",
-       price: "$123  $123.00",
+    image: blueshirt,
+    name: "Product Name Goes Here",
+    price: "$123  $123.00",
   },
   {
-       image: lamp,
-       name: "Product Name Goes Here",
-       price: "$123  $123.00",
+    image: lamp,
+    name: "Product Name Goes Here",
+    price: "$123  $123.00",
   },
   {
-       image: nikeshoes,
-       name: "Product Name Goes Here",
-       price: "$123  $123.00",
+    image: nikeshoes,
+    name: "Product Name Goes Here",
+    price: "$123  $123.00",
   },
   // {
   //      image: dron,
@@ -62,10 +62,10 @@ const products = [
 
 
 const ShopDetail = () => {
-    
-return (
-      <div className="detail-cont">
-            <div className="for-sections-link">
+
+  return (
+    <div className="detail-cont">
+      <div className="for-sections-link">
         <h4>Home</h4>
         <span>/</span>
         <h4>Shop</h4>
@@ -79,52 +79,52 @@ return (
         <h2>Product Name Goes Here</h2>
         <h4>$150.00</h4>
         <p>Volup erat ipsum diam elitr rebum et dolor.
-           Est nonumy elitr erat diam stet sit clita ea. <br /> Sanc ipsum et, labore clita lorem magna duo dolor no sea Nonumy</p>
+          Est nonumy elitr erat diam stet sit clita ea. <br /> Sanc ipsum et, labore clita lorem magna duo dolor no sea Nonumy</p>
       </div>
       <div className="for-description">
-       
+
         <div className="box-reviews">
           <button>Description</button>
           <button>Information</button>
           <button>Reviews (0)</button>
         </div>
         <h2>Product Description</h2>
-        <h5>Eos no lorem eirmod diam diam, eos elitr et gubergren diam sea. Consetetur vero aliquyam invidunt duo dolores et duo sit. Vero diam ea vero et dolore rebum, dolor rebum eirmod consetetur invidunt sed sed et, lorem duo et eos elitr, sadipscing kasd ipsum rebum diam. Dolore diam stet rebum sed tempor kasd eirmod. Takimata kasd ipsum accusam sadipscing, eos dolores sit no ut diam consetetur duo justo est, sit sanctus diam tempor aliquyam eirmod nonumy rebum dolor accusam, ipsum kasd eos consetetur at sit 
+        <h5>Eos no lorem eirmod diam diam, eos elitr et gubergren diam sea. Consetetur vero aliquyam invidunt duo dolores et duo sit. Vero diam ea vero et dolore rebum, dolor rebum eirmod consetetur invidunt sed sed et, lorem duo et eos elitr, sadipscing kasd ipsum rebum diam. Dolore diam stet rebum sed tempor kasd eirmod. Takimata kasd ipsum accusam sadipscing, eos dolores sit no ut diam consetetur duo justo est, sit sanctus diam tempor aliquyam eirmod nonumy rebum dolor accusam, ipsum kasd eos consetetur at sit
           rebum, diam kasd invidunt tempor lorem, ipsum lorem elitr sanctus eirmod takimata dolor ea invidunt.</h5>
-          <h5 id="info-padding" >Dolore magna est eirmod sanctus dolor, amet diam et eirmod et ipsum. Amet dolore tempor consetetur sed lorem 
-            dolor sit lorem tempor. Gubergren amet amet labore sadipscing clita clita diam clita. Sea amet et sed ipsum lorem elitr et, amet et labore voluptua sit rebum. Ea erat sed et diam takimata sed justo. Magna takimata justo et amet magna et.</h5>
+        <h5 id="info-padding" >Dolore magna est eirmod sanctus dolor, amet diam et eirmod et ipsum. Amet dolore tempor consetetur sed lorem
+          dolor sit lorem tempor. Gubergren amet amet labore sadipscing clita clita diam clita. Sea amet et sed ipsum lorem elitr et, amet et labore voluptua sit rebum. Ea erat sed et diam takimata sed justo. Magna takimata justo et amet magna et.</h5>
       </div>
       <div className="for-blocks-header-shop">
-                  <h1 className="box-blocks-shop">YOU MAY ALSO LIKE-----------------------------------------------------------------------------------</h1>
-                  
-              </div>
-              <div className="features-blocks-shop">
-                    {products.map((item, index) => (
-                         <div className="boxes-features">
-                            
-                              <img className="features-img" style={{ width: '270px', height: "240px", }} src={item.image} alt="#" />
-                               <div className="for-opacity">
-                                   <img src={search} alt="#" />
-                                   <img src={heart} alt="#" />
-                                   <img src={storee} alt="#" />
-                                   <img src={changee} alt="#" />
-                               </div>
-                              <div className="hover"></div>
-                              <div className="features-info">
+        <h1 className="box-blocks-shop">YOU MAY ALSO LIKE-----------------------------------------------------------------------------------</h1>
 
-                                  <h3>{item.name}</h3> 
-                                   <div>
-                                  <h5> {item.price}</h5>
-                                   </div>
-
-                              </div>
-
-                       
-                     </div>
-                    ))}
-               </div>
       </div>
+      <div className="features-blocks-shop">
+        {products.map((item, index) => (
+          <div className="boxes-features">
+
+            <img className="features-img" style={{ width: '270px', height: "240px", }} src={item.image} alt="#" />
+            <div className="for-opacity">
+              <img src={search} alt="#" />
+              <img src={heart} alt="#" />
+              <img src={storee} alt="#" />
+              <img src={changee} alt="#" />
+            </div>
+            <div className="hover"></div>
+            <div className="features-info">
+
+              <h3>{item.name}</h3>
+              <div>
+                <h5> {item.price}</h5>
+              </div>
+
+            </div>
+
+
+          </div>
+        ))}
+      </div>
+    </div>
   )
 };
-  
+
 export default ShopDetail;
