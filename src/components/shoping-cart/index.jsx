559f167f43a5eed163b11c35609168f5731f3d51
armansafaryan01 <armansafaryan75@gmail.com>
2022-08-22 18:46:03 +0400

    add

--- src/components/shoping-cart/index.jsx
+++ src/components/shoping-cart/index.jsx
@@ -4,14 +4,105 @@ import nikon from "..//..//assets/icons/nikon.jpg";
 import blueshirt from "..//..//assets/icons/blueshirt.jpg"
 import lamp from "..//..//assets/icons/night lamp.jpg"
 import { useState } from 'react';
+import { useEffect, } from 'react';
+import { NavLink, useNavigate } from "react-router-dom";
 import nike from "..//..//assets/icons/nikeshoes.jpg"
 import dron from "..//..//assets/icons/dron.jpg"
 
 const Shopcart = () => {
+  const navigate = useNavigate()
+  const GoTo = () => {
+
+
+  }
+
+
+
+  const [items, setItems] = useState([
+    {
+      img: nikon,
+      h4: 'Product Name',
+      p: '150',
+      counter: 3,
+    },
+    {
+      img: dron,
+      h4: 'Product Name ',
+      p: '320',
+      counter: 2,
+    },
+    {
+      img: nike,
+      h4: 'Product Name',
+      p: '150',
+      counter: 3,
+    },
+    {
+      img: blueshirt,
+      h4: 'Product Name ',
+      p: '320',
+      counter: 2,
+    },
+    {
+      img: nikon,
+      h4: 'Product Name',
+      p: '150',
+      counter: 3,
+    },
+
+    {
+      img: lamp,
+      h4: 'Product Name',
+      p: '150',
+      counter: 3,
+    },
+
+    {
+      img: nikon,
+      h4: 'Product Name ',
+      p: '320',
+      counter: 2,
+    },
+
+
+  ])
+  const filtr = (indes, id) => {
+
+    setItems(items.filter((vel, x) => { return id !== x && vel !== indes }))
+
+
+  }
+
+
+
+
+  let newPost1 = items.reduce((sum, item) => (sum += (item.p * item.counter)), 0)
+
+  useEffect(() => {
+
+
+  }, [items])
+  const forsis = (is, indexs) => {
+
+
+
+    setItems(items => items.map((item, itemIndex) => {
+      if (itemIndex === indexs)
+        return ({ ...item, counter: is ? item.counter + 1 : item.counter - 1 })
+      else return item
+    }))
+
+  }
+  const zoom = () => {
+    let x = items
+    x.id = 365
+    setItems(x)
+  }
   const [get, set] = useState(1)
 
   const foo = () => {
     set(get + 1)
+    zoom()
   }
 
   const foo1 = () => {
@@ -20,138 +111,58 @@ const Shopcart = () => {
     }
   }
 
+  return < div>
 
-  return (
-    <div className='contact-cont'>
+    <div className="App-G-flex" style={{ background: 'whitesmoke', fontSize: "30px", color: ' gray' }}>
       <div className="for-sections-link">
         <h4>Home</h4>
         <span>/</span>
         <h4>Shop</h4>
         <span>/</span>
-        <h4 id="shop-list">Shopping Cart</h4>
+        <h4 id="shop-list">Shop Detail</h4>
       </div>
-      <div className='container-products-g'>
-        <div className='box-buy-products'>
-          <div className='header-bar-cont'>
-            <h4>Products</h4>
-            <h4>Price</h4>
-            <h4>Quantity</h4>
-            <h4>Total</h4>
-            <h4>Remove</h4>
-          </div>
-          <div className='camera-box'>
-            <div className='name-product'>
-              <img src={nikon} alt="#" />
-              <h4>Product Name</h4>
-            </div>
-            <div className='box-price'>
-              <p>$150</p>
-            </div>
-            <div className='for-quanity'>
-              <div className="for-payments-quality">
-                <button className='function-btn' onClick={foo1}>-</button>
-                <button id="value-qnt">{get}</button>
-                <button className='function-btn' onClick={foo}>+</button>
-              </div>
-            </div>
-            <div className='total-price'>
-              <p>$150</p>
-            </div>
-            <div className='remove-btn'>
-              <button>✖</button>
-            </div>
-          </div>
-          <div className='shirt-box'>
-            <div className='name-product'>
-              <img src={blueshirt} alt="#" />
-              <h4>Product Name</h4>
-            </div>
-            <div className='box-price'>
-              <p>$150</p>
-            </div>
-            <div className='for-quanity'>
-              <div className="for-payments-quality">
-                <button className='function-btn' onClick={foo1}>-</button>
-                <button id="value-qnt">{get}</button>
-                <button className='function-btn' onClick={foo}>+</button>
-              </div>
-            </div>
-            <div className='total-price'>
-              <p>$150</p>
-            </div>
-            <div className='remove-btn'>
-              <button>✖</button>
-            </div>
-          </div>
-          <div className='lamp-box'>
-            <div className='name-product'>
-              <img src={lamp} alt="#" />
-              <h4>Product Name</h4>
-            </div>
-            <div className='box-price'>
-              <p>$150</p>
-            </div>
-            <div className='for-quanity'>
-              <div className="for-payments-quality">
-                <button className='function-btn' onClick={foo1}>-</button>
-                <button id="value-qnt">{get}</button>
-                <button className='function-btn' onClick={foo}>+</button>
-              </div>
-            </div>
-            <div className='total-price'>
-              <p>$150</p>
-            </div>
-            <div className='remove-btn'>
-              <button>✖</button>
-            </div>
-          </div>
-          <div className='nike-box'>
-            <div className='name-product'>
-              <img src={nike} alt="#" />
-              <h4>Product Name</h4>
-            </div>
-            <div className='box-price'>
-              <p>$150</p>
-            </div>
-            <div className='for-quanity'>
-              <div className="for-payments-quality">
-                <button className='function-btn' onClick={foo1}>-</button>
-                <button id="value-qnt">{get}</button>
-                <button className='function-btn' onClick={foo}>+</button>
-              </div>
-            </div>
-            <div className='total-price'>
-              <p>$150</p>
-            </div>
-            <div className='remove-btn'>
-              <button>✖</button>
-            </div>
-          </div>
-          <div className='dron-box'>
-            <div className='name-product'>
-              <img src={dron} alt="#" />
-              <h4>Product Name</h4>
-            </div>
-            <div className='box-price'>
-              <p>$150</p>
-            </div>
-            <div className='for-quanity'>
-              <div className="for-payments-quality">
-                <button className='function-btn' onClick={foo1}>-</button>
-                <button id="value-qnt">{get}</button>
-                <button className='function-btn' onClick={foo}>+</button>
-              </div>
-            </div>
-            <div className='total-price'>
-              <p>$150</p>
-            </div>
-            <div className='remove-btn'>
-              <button>✖</button>
-            </div>
-          </div>
 
+    </div>
+
+    <div className="App-cartis">
+      <div className="ada">
+        <div className="G-flex  aria">
+          <h4>Products</h4>
+          <h4>Price</h4>
+          <h4>Quantity</h4>
+          <h4>Total</h4>
+          <h4>Remove</h4>
         </div>
-        {/* ==================================================== */}
+
+
+        {items.map((item, index) => {
+          return <div className='camera-box' kay={index}>
+            <div className='name-product'>
+              <img src={item.img} alt="#" />
+              <h4>{item.h4}</h4>
+            </div>
+            <div className='box-price'>
+              <p>${item.p}</p>
+            </div>
+            <div className='for-quanity'>
+              <div className="for-payments-quality">
+                <button className='function-btn' onClick={() => forsis(false, index)}>-</button>
+                {/* <input id="value-qnt" value={item.counter} /> */}
+                <button id="value-qnt">{item.counter}</button>
+                <button className='function-btn' onClick={() => forsis(true, index)}>+</button>
+              </div>
+            </div>
+            <div className='total-price'>
+              <p>${item.p * item.counter}</p>
+            </div>
+            <div className='remove-btn'>
+              <button onClick={() => filtr(item, index)} >✖</button>
+            </div>
+          </div>
+        })}
+      </div>
+
+      <div className="g-flex" style={{ width: '40%', marginLeft: '25px' }}>
         <div className='box-checkout'>
           <div className='box-cupon-input'>
             <label>
@@ -159,31 +170,41 @@ const Shopcart = () => {
               <button>Apply Coupon</button>
             </label>
           </div>
-          <h2>CART SUMMARY------------------------------</h2>
+          <div className="D-after">
+            <h2>CART SUMMARY</h2>
+          </div>
           <div className='cont-checkout'>
             <div className='box-topic-checkout'>
 
               <div className='Subtotal-box'>
                 <h4>Subtotal</h4>
-                <p>$150</p>
+                <p >${newPost1 + '   /    '}   -10% { }</p>
               </div>
               <div className='Subtotal-box'>
                 <h4>Shipping</h4>
-                <p>$10</p>
+                <p>{newPost1 < 10000 ? '$150' : "$75"}</p>
               </div>
-              -------------------------------------------------------------
+              <div className="AFTER">
+                <div></div>
+              </div>
               <div className='Subtotal-box'>
                 <h4>Total</h4>
-                <p>$160</p>
+                <p>${newPost1 * 0.9 + +`${newPost1 < 10000 ? 150 : 75}`}</p>
               </div>
-              <button>Processed To Checkout</button>
+              <button onClick={GoTo}>Processed To Checkout</button>
 
             </div>
           </div>
         </div>
       </div>
+
     </div>
-  )
-};
+  </div>
+}
+
+
+
+
+
 
 export default Shopcart;
